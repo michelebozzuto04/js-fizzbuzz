d46f78fa55c99401333c7ae8ae1a4002ab5072de
michelebozzuto04 <michele.bozzuto04@gmail.com>
2025-05-28 15:41:18 +0200

    Implement FizzBuzz logic in main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("prova")
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0) {
+        if (i % 5 == 0) {
+            console.log(i, "FizzBuzz");
+        } else {
+            console.log(i, "Fizz");
+        }
+    } else if (i % 5 == 0) {
+        console.log(i, "Buzz");
+    } else {
+        console.log(i)
+    }
+}
